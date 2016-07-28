b136668a2fb3bae940118608c98c1a8b36335c97
Vincent Tran <vincenttran23@gmail.com>
2016-07-28 16:42:07 -0400

    about halfway done with retirement calculator form

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ app.use(express.static(__dirname + '/public')); //serve static content
 
 // Homepage
 app.get('/', function(req, res) {
+	res.set('Content-Type', 'text/html');
 	return res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/retirement', function(req, res) {
+	res.set('Content-Type', 'text/html');
+	return res.sendFile(__dirname + '/public/retirement.html');
+});
+
+app.post('/retirement', function(req, res) {
+
 });
 
 app.listen(process.env.PORT || 3000);
