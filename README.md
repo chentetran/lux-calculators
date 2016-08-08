b003ce40802445274d143725e148503b910df715
Vincent Tran <vincenttran23@gmail.com>
2016-08-08 01:00:07 -0400

    finished first retirement calculator

---
+++ README.md
@@ -0,0 +1,5 @@
+## Financial Calculators
+
+# API documentation
+
+
--- index.js
+++ index.js
@@ -17,13 +17,57 @@ app.get('/retirement', function(req, res) {
 	return res.sendFile(__dirname + '/public/retirement.html');
 });
 
+// Route for determining how much money user needs for retirement
 app.post('/retirement', function(req, res) {
+	const {
+		annualIncome,
+		age,
+		lifeExpectancy,
+		ageOfRetirement,
+		inflation,
+		wageIncrease,
+		avgAnnualReturn
+	} = req.body;
 
-	const { annualIncome, age } = req.body;
-	console.log(annualIncome)
-	console.log(age)
+	/***** Some helper variables *****/
+	const inflationPlusWageIncrease = 
+		inflation / 100 + wageIncrease / 100;
+	const sum_inflation_wageIncrase_avgAnnualReturn =
+		inflationPlusWageIncrease + avgAnnualReturn / 100;
+	const yearsUntilRetirement =
+		ageOfRetirement - age;
+	/*********************************/
 
-	res.send(annualIncome);
+	// How much you need to earn by retirement to maintain your lifestyle
+	const earnByRetirement = 
+		annualIncome * 
+		Math.pow((1 + inflationPlusWageIncrease), yearsUntilRetirement) *
+		.80;
+
+	// How much money you need in your retirement fund
+	const neededInRetirementFund = 
+		earnByRetirement *
+		(
+			(
+				Math.pow((1 + inflationPlusWageIncrease), lifeExpectancy - ageOfRetirement) - 1
+			) / inflationPlusWageIncrease
+		);
+
+	// How much you need to save each year
+	const saveEachYear = 
+		neededInRetirementFund * sum_inflation_wageIncrase_avgAnnualReturn / 
+		(Math.pow(1 + sum_inflation_wageIncrase_avgAnnualReturn, yearsUntilRetirement) - 1);
+
+	// Percentage of your income
+	const percentOfIncome = 
+		(saveEachYear / annualIncome * 100).toFixed(2)
+
+	res.send({
+		earnByRetirement,
+		neededInRetirementFund,
+		saveEachYear,
+		percentOfIncome
+	});
 });
 
 app.listen(process.env.PORT || 3000);
